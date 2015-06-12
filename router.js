b399873bed452262781618eca6c4c628ea687bb4
mooshroom <273940748@qq.com>
2015-06-12 10:38:09 +0800

    路由自动构建函数BUG修复

--- router.js
+++ router.js
@@ -4,22 +4,16 @@ avalon.scan();
 require(["mmRouter", "mmRequest", "nav"], function () {
 
     //根据UI表创建对应的路由和模块配置
-    function buildRouter(l){
-        for(var i=0;i< l.length;i++){
-            var n= l[i].name
-            var path="../../ui/"+n+"/"+n+'.js'
-
-            //构建路由
+        function newRouter(n){
             avalon.router.get('/'+ n, function () {
                 modal.getOutByRouter()
                 layout.url = "./ui/"+ n+"/doc.html"
-                require([path],function(){
+                require(["../../ui/"+n+"/"+n+'.js'],function(){
                     console.log(n+"加载成功")
                 })
             });
-            console.log(n)
         }
-    }
+
 
 
 
@@ -48,12 +42,21 @@ require(["mmRouter", "mmRequest", "nav"], function () {
         //加载所依赖的VM
     });
 
+    //获取ui列表
     avalon.ajax({
         type: "get",
         url: "./ui.json",
         success: function (res) {
             cache.go(res)
-            buildRouter(res.uiList)
+
+            //生成路由
+            for(var i=0;i< res.uiList.length;i++){
+                var n= res.uiList[i].name
+                //构建路由
+                newRouter(n)
+                console.log(n)
+            }
+            //开始监听路由
             avalon.history.start();
         }
     })
